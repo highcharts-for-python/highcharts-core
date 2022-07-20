60cf216cbd173ee67e3e3e880e0bd1c45757b7e0
Chris Modzelewski <software@insightindustry.com>
2022-07-20 12:19:55 -0400

    Implemented SolidGaugeOptions.

--- highcharts/plot_options/gauge.py
+++ highcharts/plot_options/gauge.py
@@ -7,6 +7,7 @@ from highcharts import errors
 from highcharts.decorators import class_sensitive, validate_types
 from highcharts.metaclasses import HighchartsMeta
 from highcharts.plot_options.generic import GenericTypeOptions
+from highcharts.plot_options.series import SeriesOptions
 from highcharts.utility_classes.gradients import Gradient
 from highcharts.utility_classes.patterns import Pattern
 from highcharts.utility_classes.shadows import ShadowOptions
@@ -814,6 +815,199 @@ class GaugeOptions(GenericTypeOptions):
             'wrap': self.wrap
         }
         parent_as_dict = super().to_dict()
+
+        for key in parent_as_dict:
+            untrimmed[key] = parent_as_dict[key]
+
+        return self.trim_dict(untrimmed)
+
+
+class SolidGauge(SeriesOptions):
+    """General options to apply to all Solid Gauge series types.
+
+    A solid gauge is a circular gauge where the value is indicated by a filled arc,
+    and the color of the arc may variate with the value.
+
+    .. figure:: _static/solidgauge-example.png
+      :alt: SolidGauge Example Chart
+      :align: center
+
+    """
+
+    def __init__(self, **kwargs):
+        self._inner_radius = None
+        self._overshoot = None
+        self._radius = None
+        self._rounded = None
+
+        self.inner_radius = kwargs.pop('inner_radius', None)
+        self.overshoot = kwargs.pop('overshoot', None)
+        self.radius = kwargs.pop('radius', None)
+        self.rounded = kwargs.pop('rounded', None)
+
+        super().__init__(**kwargs)
+
+    @property
+    def inner_radius(self) -> Optional[str | int | float | Decimal]:
+        """The inner radius for points in a solid gauge. Can be given as a number (pixels)
+        or percentage string. Defaults to ``60``.
+
+        :rtype: numeric, :class:`str <python:str>`, or :obj:`None <python:None>`
+        """
+        return self._inner_radius
+
+    @inner_radius.setter
+    def inner_radius(self, value):
+        if value is None:
+            self._inner_radius = None
+        else:
+            try:
+                value = validators.string(value)
+                if '%' not in value:
+                    raise ValueError
+            except ValueError:
+                value = validators.numeric(value, minimum = 0)
+
+            self._inner_radius = value
+
+    @property
+    def overshoot(self) -> Optional[int | float | Decimal]:
+        """Allow the dial to overshoot the end of the perimeter axis by this many degrees.
+
+        For example, if this option is set to ``5`` and the gauge axis goes from ``0`` to
+        ``60``, a value of ``100``, or ``1000``, will show ``5`` degrees beyond the end of
+        the axis.
+
+        Defaults to ``0``.
+
+        :rtype: numeric or :obj:`None <python:None>`
+        """
+        return self._overshoot
+
+    @overshoot.setter
+    def overshoot(self, value):
+        self._overshoot = validators.numeric(value,
+                                             allow_empty = True,
+                                             minimum = 0)
+
+    @property
+    def radius(self) -> Optional[str | int | float | Decimal]:
+        """The outer radius for points in a solid gauge. Can be given as a number (pixels)
+        or percentage string. Defaults to ``100``.
+
+        :rtype: numeric, :class:`str <python:str>`, or :obj:`None <python:None>`
+        """
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value is None:
+            self._radius = None
+        else:
+            try:
+                value = validators.string(value)
+                if '%' not in value:
+                    raise ValueError
+            except ValueError:
+                value = validators.numeric(value, minimum = 0)
+
+            self._radius = value
+
+    @property
+    def rounded(self) -> Optional[bool]:
+        """If ``True``, draws rounded edges on the gauge. Defaults to ``False``.
+
+        :rtype: :class:`bool <python:bool>` or :obj:`None <python:None>`
+        """
+        return self._rounded
+
+    @rounded.setter
+    def rounded(self, value):
+        if value is None:
+            self._rounded = None
+        else:
+            self._rounded = bool(value)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'accessibility': as_dict.pop('accessibility', None),
+            'allow_point_select': as_dict.pop('allowPointSelect', False),
+            'animation': as_dict.pop('animation', None),
+            'class_name': as_dict.pop('className', None),
+            'clip': as_dict.pop('clip', True),
+            'color': as_dict.pop('color', None),
+            'cursor': as_dict.pop('cursor', None),
+            'custom': as_dict.pop('custom', None),
+            'dash_style': as_dict.pop('dashStyle', None),
+            'data_labels': as_dict.pop('dataLabels', None),
+            'description': as_dict.pop('description', None),
+            'enable_mouse_tracking': as_dict.pop('enableMouseTracking', True),
+            'events': as_dict.pop('events', None),
+            'include_in_data_export': as_dict.pop('includeInDataExport', None),
+            'keys': as_dict.pop('keys', None),
+            'label': as_dict.pop('label', None),
+            'linked_to': as_dict.pop('linkedTo', None),
+            'marker': as_dict.pop('marker', None),
+            'on_point': as_dict.pop('onPoint', None),
+            'opacity': as_dict.pop('opacity', None),
+            'point': as_dict.pop('point', None),
+            'point_description_formatter': as_dict.pop('pointDescriptionFormatter', None),
+            'selected': as_dict.pop('selected', False),
+            'show_checkbox': as_dict.pop('showCheckbox', False),
+            'show_in_legend': as_dict.pop('showInLegend', None),
+            'skip_keyboard_navigation': as_dict.pop('skipKeyboardNavigation', None),
+            'states': as_dict.pop('states', None),
+            'threshold': as_dict.pop('threshold', None),
+            'tooltip': as_dict.pop('tooltip', None),
+            'turbo_threshold': as_dict.pop('turboThreshold', None),
+            'visible': as_dict.pop('visible', True),
+
+            'animation_limit': as_dict.pop('animationLimit', None),
+            'boost_blending': as_dict.pop('boostBlending', None),
+            'boost_threshold': as_dict.pop('boostThreshold', 5000),
+            'color_axis': as_dict.pop('colorAxis', None),
+            'color_index': as_dict.pop('colorIndex', None),
+            'color_key': as_dict.pop('colorKey', None),
+            'connect_ends': as_dict.pop('connectEnds', None),
+            'connect_nulls': as_dict.pop('connectNulls', False),
+            'crisp': as_dict.pop('crisp', True),
+            'crop_threshold': as_dict.pop('cropThreshold', 300),
+            'data_sorting': as_dict.pop('dataSorting', None),
+            'drag_drop': as_dict.pop('dragDrop', None),
+            'find_nearest_point_by': as_dict.pop('findNearestPointBy', None),
+            'get_extremes_for_all': as_dict.pop('getExtremesForAll', False),
+            'linecap': as_dict.pop('linecap', 'round'),
+            'line_width': as_dict.pop('lineWidth', 2),
+            'negative_color': as_dict.pop('negativeColor', None),
+            'point_interval': as_dict.pop('pointInterval', 1),
+            'point_interval_unit': as_dict.pop('pointIntervalUnit', None),
+            'point_placement': as_dict.pop('pointPlacement', None),
+            'point_start': as_dict.pop('pointStart', 0),
+            'relative_x_value': as_dict.pop('relativeXValue', False),
+            'shadow': as_dict.pop('shadow', False),
+            'soft_threshold': as_dict.pop('softThreshold', True),
+            'stacking': as_dict.pop('stacking', None),
+            'step': as_dict.pop('step', None),
+            'zone_axis': as_dict.pop('zoneAxis', 'y'),
+            'zones': as_dict.pop('zones', None),
+
+            'inner_radius': as_dict.pop('innerRadius', None),
+            'overshoot': as_dict.pop('overshoot', None),
+            'radius': as_dict.pop('radius', None),
+            'rounded': as_dict.pop('rounded', None)
+        }
+
+        return kwargs
+
+    def to_dict(self) -> dict:
+        untrimmed = {
+            'inner_radius': self.inner_radius,
+            'overshoot': self.overshoot,
+            'radius': self.radius,
+            'rounded': self.rounded
+        }
+        parent_as_dict = super(self).to_dict()
 
         for key in parent_as_dict:
             untrimmed[key] = parent_as_dict[key]
