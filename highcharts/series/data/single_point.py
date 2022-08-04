de291d6e02a2f82768c4a5828d32aca05aea2cc4
Chris Modzelewski <software@insightindustry.com>
2022-08-04 16:21:59 -0400

    Implemented TimelineSeries and related.

--- highcharts/series/data/single_point.py
+++ highcharts/series/data/single_point.py
@@ -10,19 +10,17 @@ from highcharts.plot_options.drag_drop import DragDropOptions
 from highcharts.utility_classes.data_labels import DataLabel
 
 
-class SinglePointData(DataBase):
+class SinglePointBase(DataBase):
     """Data point that features a single and ``y`` value."""
 
     def __init__(self, **kwargs):
         self._data_labels = None
         self._drag_drop = None
         self._drilldown = None
-        self._y = None
 
         self.data_labels = kwargs.pop('data_labels', None)
         self.drag_drop = kwargs.pop('drag_drop', None)
         self.drilldown = kwargs.pop('drilldown', None)
-        self.y = kwargs.pop('y', None)
 
         super().__init__(**kwargs)
 
@@ -73,6 +71,71 @@ class SinglePointData(DataBase):
     @drilldown.setter
     def drilldown(self, value):
         self._drilldown = validators.string(value, allow_empty = True)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        """Convenience method which returns the keyword arguments used to initialize the
+        class from a Highcharts Javascript-compatible :class:`dict <python:dict>` object.
+
+        :param as_dict: The HighCharts JS compatible :class:`dict <python:dict>`
+          representation of the object.
+        :type as_dict: :class:`dict <python:dict>`
+
+        :returns: The keyword arguments that would be used to initialize an instance.
+        :rtype: :class:`dict <python:dict>`
+
+        """
+        kwargs = {
+            'accessibility': as_dict.pop('accessibility', None),
+            'class_name': as_dict.pop('className', None),
+            'color': as_dict.pop('color', None),
+            'color_index': as_dict.pop('colorIndex', None),
+            'custom': as_dict.pop('custom', None),
+            'description': as_dict.pop('description', None),
+            'events': as_dict.pop('events', None),
+            'id': as_dict.pop('id', None),
+            'label_rank': as_dict.pop('labelrank', None),
+            'name': as_dict.pop('name', None),
+            'selected': as_dict.pop('selected', None),
+
+            'data_labels': as_dict.pop('dataLabels', None),
+            'drag_drop': as_dict.pop('dragDrop', None),
+            'drilldown': as_dict.pop('drilldown', None),
+        }
+
+        return kwargs
+
+    def to_dict(self) -> Optional[dict]:
+        untrimmed = {
+            'accessibility': self.accessibility,
+            'className': self.class_name,
+            'color': self.color,
+            'colorIndex': self.color_index,
+            'custom': self.custom,
+            'description': self.description,
+            'events': self.events,
+            'id': self.id,
+            'labelrank': self.label_rank,
+            'name': self.name,
+            'selected': self.selected,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+        }
+
+        return self.trim_dict(untrimmed)
+
+
+class SinglePointData(SinglePointBase):
+    """Data point that features a single and ``y`` value."""
+
+    def __init__(self, **kwargs):
+        self._y = None
+
+        self.y = kwargs.pop('y', None)
+
+        super().__init__(**kwargs)
 
     @property
     def y(self) -> Optional[int | float | Decimal | type(None) | constants.EnforcedNullType]:
@@ -151,6 +214,7 @@ class SinglePointData(DataBase):
             'data_labels': as_dict.pop('dataLabels', None),
             'drag_drop': as_dict.pop('dragDrop', None),
             'drilldown': as_dict.pop('drilldown', None),
+
             'y': as_dict.pop('y', None),
         }
 
@@ -173,75 +237,22 @@ class SinglePointData(DataBase):
             'dataLabels': self.data_labels,
             'dragDrop': self.drag_drop,
             'drilldown': self.drilldown,
+
             'y': self.y,
         }
 
         return self.trim_dict(untrimmed)
 
 
-class SingleValueData(DataBase):
+class SingleValueData(SinglePointBase):
     """Data point that features a single and ``value`` value."""
 
     def __init__(self, **kwargs):
-        self._data_labels = None
-        self._drag_drop = None
-        self._drilldown = None
         self._value = None
 
-        self.data_labels = kwargs.pop('data_labels', None)
-        self.drag_drop = kwargs.pop('drag_drop', None)
-        self.drilldown = kwargs.pop('drilldown', None)
         self.value = kwargs.pop('value', None)
 
         super().__init__(**kwargs)
-
-    @property
-    def data_labels(self) -> Optional[DataLabel]:
-        """Individual data label for the data point.
-
-        :rtype: :class:`DataLabel` or :obj:`None <python:None>`
-        """
-        return self._data_labels
-
-    @data_labels.setter
-    @class_sensitive(DataLabel)
-    def data_labels(self, value):
-        self._data_labels = value
-
-    @property
-    def drag_drop(self) -> Optional[DragDropOptions]:
-        """The draggable-points module allows points to be moved around or modified in the
-        chart.
-
-        In addition to the options mentioned under the dragDrop API structure, the module
-        fires three (JavaScript) events:
-
-          * ``point.dragStart``
-          * ``point.drag``
-          * ``point.drop``
-
-        :rtype: :class:`DragDropOptions` or :obj:`None <python:None>`
-        """
-        return self._drag_drop
-
-    @drag_drop.setter
-    @class_sensitive(DragDropOptions)
-    def drag_drop(self, value):
-        self._drag_drop = value
-
-    @property
-    def drilldown(self) -> Optional[str]:
-        """The :meth:`id <SeriesBase.id>` of a series in the ``drilldown.series`` array
-        to use as a drilldown destination for this point. Defaults to
-        :obj:`None <python:None>`.
-
-        :rtype: :class:`str <python:str>` or :obj:`None <python:None>`
-        """
-        return self._drilldown
-
-    @drilldown.setter
-    def drilldown(self, value):
-        self._drilldown = validators.string(value, allow_empty = True)
 
     @property
     def value(self) -> Optional[int | float | Decimal | type(None) | constants.EnforcedNullType]:
@@ -343,6 +354,139 @@ class SingleValueData(DataBase):
             'dragDrop': self.drag_drop,
             'drilldown': self.drilldown,
             'value': self.value,
+        }
+
+        return self.trim_dict(untrimmed)
+
+
+class SingleXData(SinglePointBase):
+    """Data point that features a single labeled ``x`` value."""
+
+    def __init__(self, **kwargs):
+        self._label = None
+        self._x = None
+
+        self.label = kwargs.pop('label', None)
+        self.x = kwargs.pop('x', None)
+
+        super().__init__(**kwargs)
+
+    @property
+    def label(self) -> Optional[str]:
+        """The label for the event. Defaults to :obj:`None <python:None>`.
+
+        :rtype: :class:`str <python:str>` or :obj:`None <python:None>`
+        """
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        self._label = validators.string(value, allow_empty = True)
+
+    @property
+    def x(self) -> Optional[int | float | Decimal | type(None) | constants.EnforcedNullType]:
+        """The position of the data point on the X-axis. Defaults to
+        :obj:`None <python:None>`.
+
+        :rtype: numeric or :class:`EnforcedNullType` or :obj:`None <python:None>`
+        """
+        return self._y
+
+    @x.setter
+    def x(self, value):
+        if value is None or isinstance(value, constants.EnforcedNullType):
+            self._x = None
+        else:
+            self._x = validators.numeric(value)
+
+    @classmethod
+    def from_setter(cls, value):
+        """Generator method which produces a collection of :class:`SinglePointData`
+        instances derived from ``value``. Generally consumed by the setter methods in
+        series-type specific data classes.
+
+        :rtype: :class:`list <python:list>` of :obj:`SinglePointData` instances
+        """
+        if not value:
+            return []
+        elif not checkers.is_iterable(value):
+            value = [value]
+
+        collection = []
+        for item in value:
+            if checkers.is_type(item, 'SinglePointData'):
+                as_obj = item
+            elif checkers.is_dict(item):
+                as_obj = cls.from_dict(item)
+            elif item is None or isinstance(item, constants.EnforcedNullType):
+                as_obj = cls(x = None)
+            elif checkers.is_numeric(item):
+                as_obj = cls(x = item)
+            else:
+                raise errors.HighchartsValueError(f'each data point supplied must either '
+                                                  f'be a Single X-value Data Point or be '
+                                                  f'coercable to one. Could not coerce: '
+                                                  f'{item}')
+            collection.append(as_obj)
+
+        return collection
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        """Convenience method which returns the keyword arguments used to initialize the
+        class from a Highcharts Javascript-compatible :class:`dict <python:dict>` object.
+
+        :param as_dict: The HighCharts JS compatible :class:`dict <python:dict>`
+          representation of the object.
+        :type as_dict: :class:`dict <python:dict>`
+
+        :returns: The keyword arguments that would be used to initialize an instance.
+        :rtype: :class:`dict <python:dict>`
+
+        """
+        kwargs = {
+            'accessibility': as_dict.pop('accessibility', None),
+            'class_name': as_dict.pop('className', None),
+            'color': as_dict.pop('color', None),
+            'color_index': as_dict.pop('colorIndex', None),
+            'custom': as_dict.pop('custom', None),
+            'description': as_dict.pop('description', None),
+            'events': as_dict.pop('events', None),
+            'id': as_dict.pop('id', None),
+            'label_rank': as_dict.pop('labelrank', None),
+            'name': as_dict.pop('name', None),
+            'selected': as_dict.pop('selected', None),
+
+            'data_labels': as_dict.pop('dataLabels', None),
+            'drag_drop': as_dict.pop('dragDrop', None),
+            'drilldown': as_dict.pop('drilldown', None),
+
+            'label': as_dict.pop('label', None),
+            'x': as_dict.pop('x', None),
+        }
+
+        return kwargs
+
+    def to_dict(self) -> Optional[dict]:
+        untrimmed = {
+            'accessibility': self.accessibility,
+            'className': self.class_name,
+            'color': self.color,
+            'colorIndex': self.color_index,
+            'custom': self.custom,
+            'description': self.description,
+            'events': self.events,
+            'id': self.id,
+            'labelrank': self.label_rank,
+            'name': self.name,
+            'selected': self.selected,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
+            'label': self.label,
+            'x': self.x,
         }
 
         return self.trim_dict(untrimmed)
---
+++ highcharts/series/timeline.py
@@ -0,0 +1,148 @@
+from typing import Optional, List
+
+from highcharts.series.base import SeriesBase
+from highcharts.series.data.single_point import SingleXData
+from highcharts.plot_options.timeline import TimelineOptions
+from highcharts.utility_functions import mro_init, mro_to_dict
+
+
+class TimelineSeries(SeriesBase, TimelineOptions):
+    """General options to apply to all Timeline series types.
+
+    The timeline series presents given events along a drawn line.
+
+    .. tabs::
+
+      .. tab:: Standard Timeline
+
+        .. figure:: _static/timeline-example.png
+          :alt: Timeline Example Chart
+          :align: center
+
+      .. tab:: Inverted Timeline
+
+        .. figure:: _static/timeline-example-inverted.png
+          :alt: Inverted Timeline Example Chart
+          :align: center
+
+      .. tab:: With True Datetime Axis
+
+        .. figure:: _static/timeline-example-datetime.png
+          :alt: Timeline Example Chart with Datetime Axis
+          :align: center
+
+    """
+
+    def __init__(self, **kwargs):
+        mro_init(self, kwargs)
+
+    @property
+    def data(self) -> Optional[List[SingleXData]]:
+        """Collection of data that represents the series. Defaults to
+        :obj:`None <python:None>`.
+
+        While the series type returns a collection of :class:`SingleXData` instances,
+        it accepts as input three different types of data:
+
+        .. tabs::
+
+          .. tab:: Object Collection
+
+            A one-dimensional collection of :class:`SingleXData` objects.
+
+        :rtype: :class:`list <python:list>` of :class:`SingleXData` or
+          :obj:`None <python:None>`
+        """
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        if not value:
+            self._data = None
+        else:
+            self._data = SingleXData.from_setter(value)
+
+    @classmethod
+    def _get_kwargs_from_dict(cls, as_dict):
+        kwargs = {
+            'accessibility': as_dict.pop('accessibility', None),
+            'allow_point_select': as_dict.pop('allowPointSelect', False),
+            'animation': as_dict.pop('animation', None),
+            'class_name': as_dict.pop('className', None),
+            'clip': as_dict.pop('clip', True),
+            'color': as_dict.pop('color', None),
+            'cursor': as_dict.pop('cursor', None),
+            'custom': as_dict.pop('custom', None),
+            'dash_style': as_dict.pop('dashStyle', None),
+            'data_labels': as_dict.pop('dataLabels', None),
+            'description': as_dict.pop('description', None),
+            'enable_mouse_tracking': as_dict.pop('enableMouseTracking', True),
+            'events': as_dict.pop('events', None),
+            'include_in_data_export': as_dict.pop('includeInDataExport', None),
+            'keys': as_dict.pop('keys', None),
+            'label': as_dict.pop('label', None),
+            'linked_to': as_dict.pop('linkedTo', None),
+            'marker': as_dict.pop('marker', None),
+            'on_point': as_dict.pop('onPoint', None),
+            'opacity': as_dict.pop('opacity', None),
+            'point': as_dict.pop('point', None),
+            'point_description_formatter': as_dict.pop('pointDescriptionFormatter', None),
+            'selected': as_dict.pop('selected', False),
+            'show_checkbox': as_dict.pop('showCheckbox', False),
+            'show_in_legend': as_dict.pop('showInLegend', None),
+            'skip_keyboard_navigation': as_dict.pop('skipKeyboardNavigation', None),
+            'states': as_dict.pop('states', None),
+            'threshold': as_dict.pop('threshold', None),
+            'tooltip': as_dict.pop('tooltip', None),
+            'turbo_threshold': as_dict.pop('turboThreshold', None),
+            'visible': as_dict.pop('visible', True),
+
+            'animation_limit': as_dict.pop('animationLimit', None),
+            'boost_blending': as_dict.pop('boostBlending', None),
+            'boost_threshold': as_dict.pop('boostThreshold', 5000),
+            'color_axis': as_dict.pop('colorAxis', None),
+            'color_index': as_dict.pop('colorIndex', None),
+            'color_key': as_dict.pop('colorKey', None),
+            'connect_ends': as_dict.pop('connectEnds', None),
+            'connect_nulls': as_dict.pop('connectNulls', False),
+            'crisp': as_dict.pop('crisp', True),
+            'crop_threshold': as_dict.pop('cropThreshold', 300),
+            'data_sorting': as_dict.pop('dataSorting', None),
+            'drag_drop': as_dict.pop('dragDrop', None),
+            'find_nearest_point_by': as_dict.pop('findNearestPointBy', None),
+            'get_extremes_for_all': as_dict.pop('getExtremesForAll', False),
+            'linecap': as_dict.pop('linecap', 'round'),
+            'line_width': as_dict.pop('lineWidth', 2),
+            'negative_color': as_dict.pop('negativeColor', None),
+            'point_interval': as_dict.pop('pointInterval', 1),
+            'point_interval_unit': as_dict.pop('pointIntervalUnit', None),
+            'point_placement': as_dict.pop('pointPlacement', None),
+            'point_start': as_dict.pop('pointStart', 0),
+            'relative_x_value': as_dict.pop('relativeXValue', False),
+            'shadow': as_dict.pop('shadow', False),
+            'soft_threshold': as_dict.pop('softThreshold', True),
+            'stacking': as_dict.pop('stacking', None),
+            'step': as_dict.pop('step', None),
+            'zone_axis': as_dict.pop('zoneAxis', 'y'),
+            'zones': as_dict.pop('zones', None),
+
+            'data': as_dict.pop('data', None),
+            'id': as_dict.pop('id', None),
+            'index': as_dict.pop('index', None),
+            'legend_index': as_dict.pop('legendIndex', None),
+            'name': as_dict.pop('name', None),
+            'stack': as_dict.pop('stack', None),
+            'x_axis': as_dict.pop('xAxis', None),
+            'y_axis': as_dict.pop('yAxis', None),
+            'z_index': as_dict.pop('zIndex', None),
+
+            'color_by_point': as_dict.pop('colorByPoint', None),
+            'ignore_hidden_point': as_dict.pop('ignoreHiddenPoint', None),
+        }
+
+        return kwargs
+
+    def to_dict(self) -> dict:
+        untrimmed = mro_to_dict(self)
+
+        return self.trim_dict(untrimmed)
