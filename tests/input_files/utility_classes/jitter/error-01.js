9efa38d17ba4ab7fbba5bff1fdc215aa048bcefa
Chris Modzelewski <software@insightindustry.com>
2022-08-16 16:07:34 -0400

    Implemented unit tests for highcharts.utility_classes.jitter.

---
+++ tests/input_files/utility_classes/jitter/error-01.js
@@ -0,0 +1,4 @@
+{
+    x: 123,
+    y: 'not a number'
+}
---
+++ tests/utility_classes/test_jitter.py
@@ -0,0 +1,60 @@
+"""Tests for ``highcharts.no_data``."""
+
+import pytest
+
+from json.decoder import JSONDecodeError
+
+from highcharts.utility_classes.jitter import Jitter as cls
+from highcharts import errors
+from tests.fixtures import input_files, check_input_file, to_camelCase, to_js_dict, \
+    Class__init__, Class__to_untrimmed_dict, Class_from_dict, Class_to_dict, \
+    Class_from_js_literal
+
+STANDARD_PARAMS = [
+    ({}, None),
+    ({
+      'x': 123,
+      'y': 456
+    }, None),
+]
+
+
+@pytest.mark.parametrize('kwargs, error', STANDARD_PARAMS)
+def test__init__(kwargs, error):
+    Class__init__(cls, kwargs, error)
+
+
+@pytest.mark.parametrize('kwargs, error', STANDARD_PARAMS)
+def test__to_untrimmed_dict(kwargs, error):
+    Class__to_untrimmed_dict(cls, kwargs, error)
+
+
+@pytest.mark.parametrize('kwargs, error',  STANDARD_PARAMS)
+def test_from_dict(kwargs, error):
+    Class_from_dict(cls, kwargs, error)
+
+
+@pytest.mark.parametrize('kwargs, error',  STANDARD_PARAMS)
+def test_to_dict(kwargs, error):
+    Class_to_dict(cls, kwargs, error)
+
+
+@pytest.mark.parametrize('filename, as_file, error', [
+    ('utility_classes/jitter/01.js', False, None),
+
+    ('utility_classes/jitter/error-01.js', False, (errors.HighchartsValueError,
+                                                   errors.HighchartsParseError,
+                                                   JSONDecodeError,
+                                                   TypeError,
+                                                   ValueError)),
+
+    ('utility_classes/jitter/01.js', True, None),
+
+    ('utility_classes/jitter/error-01.js', True, (errors.HighchartsValueError,
+                                                  errors.HighchartsParseError,
+                                                  JSONDecodeError,
+                                                  TypeError,
+                                                  ValueError)),
+])
+def test_from_js_literal(input_files, filename, as_file, error):
+    Class_from_js_literal(cls, input_files, filename, as_file, error)
