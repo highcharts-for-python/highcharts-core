9a7c6879f40b4c195492498ece7ee434679f2b46
Chris Modzelewski <software@insightindustry.com>
2022-08-15 21:17:53 -0400

    Implemented unit tests for highcharts.utility_classes.shadows.

---
+++ tests/input_files/utility_classes/shadows/01.js
@@ -0,0 +1,6 @@
+{
+    color: '#cccccc',
+    offsetX: 10,
+    offsetY: 5,
+    width: 4
+}
---
+++ tests/input_files/utility_classes/shadows/error-01.js
@@ -0,0 +1,6 @@
+{
+    color: '#cccccc',
+    offsetX: 10,
+    offsetY: 'not a number',
+    width: 4
+}
