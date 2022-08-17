150ea244ca1a9eee550fdac5b87dff4fb8f67318
Chris Modzelewski <software@insightindustry.com>
2022-08-17 15:45:29 -0400

    Implemented unit tests for highcharts.utility_classes.animation.

---
+++ tests/input_files/utility_classes/animation/01.js
@@ -0,0 +1,7 @@
+{
+    complete: function () { return true; },
+    defer: 5,
+    duration: 30,
+    easing: 'easing',
+    step: function () { return true; }
+}
---
+++ tests/input_files/utility_classes/animation/error-01.js
@@ -0,0 +1,7 @@
+{
+    complete: function () { return true; },
+    defer: 5,
+    duration: 'not a number',
+    easing: 'easing',
+    step: function () { return true; }
+}
