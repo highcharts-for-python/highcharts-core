a4fd70dc635ab8258234f707f4dc4b34786c1d64
Chris Modzelewski <software@insightindustry.com>
2022-08-26 16:06:41 -0400

    Implemented unit tests for highcharts.plot_options.dependencywheel.

--- highcharts/plot_options/dependencywheel.py
+++ highcharts/plot_options/dependencywheel.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 
 from validator_collection import validators
 
-from highcharts import constants, errors
+from highcharts import constants, errors, utility_functions
 from highcharts.decorators import class_sensitive, validate_types
 from highcharts.plot_options.generic import GenericTypeOptions
 from highcharts.utility_classes.gradients import Gradient
@@ -39,20 +39,20 @@ class DependencyWheelOptions(GenericTypeOptions):
         self._node_width = None
         self._start_angle = None
 
-        self.border_color = kwargs.pop('border_color', None)
-        self.border_width = kwargs.pop('border_width', None)
-        self.center = kwargs.pop('center', None)
-        self.center_in_category = kwargs.pop('center_in_category', None)
-        self.color_by_point = kwargs.pop('color_by_point', None)
-        self.color_index = kwargs.pop('color_index', None)
-        self.colors = kwargs.pop('colors', None)
-        self.curve_factor = kwargs.pop('curve_factor', None)
-        self.levels = kwargs.pop('levels', None)
-        self.link_opacity = kwargs.pop('link_opacity', None)
-        self.min_link_width = kwargs.pop('min_link_width', None)
-        self.node_padding = kwargs.pop('node_padding', None)
-        self.node_width = kwargs.pop('node_width', None)
-        self.start_angle = kwargs.pop('start_angle', None)
+        self.border_color = kwargs.get('border_color', None)
+        self.border_width = kwargs.get('border_width', None)
+        self.center = kwargs.get('center', None)
+        self.center_in_category = kwargs.get('center_in_category', None)
+        self.color_by_point = kwargs.get('color_by_point', None)
+        self.color_index = kwargs.get('color_index', None)
+        self.colors = kwargs.get('colors', None)
+        self.curve_factor = kwargs.get('curve_factor', None)
+        self.levels = kwargs.get('levels', None)
+        self.link_opacity = kwargs.get('link_opacity', None)
+        self.min_link_width = kwargs.get('min_link_width', None)
+        self.node_padding = kwargs.get('node_padding', None)
+        self.node_width = kwargs.get('node_width', None)
+        self.start_angle = kwargs.get('start_angle', None)
 
         super().__init__(**kwargs)
 
@@ -106,14 +106,18 @@ class DependencyWheelOptions(GenericTypeOptions):
             value = validators.iterable(value)
             if len(value) != 2:
                 raise errors.HighchartsValueError(f'center expects a 2-member array. '
-                                                  f'Received a {len(value)}-member array.')
+                                                  f'Received a {len(value)}-member '
+                                                  f'array.')
             processed_values = []
             for item in value:
                 try:
-                    item = validators.string(value)
+                    item = validators.string(item)
                     if '%' not in item:
-                        raise ValueError
-                except ValueError:
+                        raise errors.HighchartsValueError('center expects a 2-member '
+                                                          'array of numbers or '
+                                                          'percentage strings. No "%" '
+                                                          'was found in the string.')
+                except TypeError:
                     item = validators.numeric(value)
 
                 processed_values.append(item)
@@ -190,16 +194,7 @@ class DependencyWheelOptions(GenericTypeOptions):
             self._colors = None
         else:
             value = validators.iterable(value)
-            checked_values = []
-            for item in value:
-                if isinstance(value, str):
-                    checked_values.append(item)
-                else:
-                    processed_item = validate_types(item,
-                                                    types = (Gradient, Pattern))
-                    checked_values.append(processed_item)
-
-            self._colors = checked_values
+            self._colors = [utility_functions.validate_color(x) for x in value]
 
     @property
     def curve_factor(self) -> Optional[int | float | Decimal]:
@@ -314,52 +309,53 @@ class DependencyWheelOptions(GenericTypeOptions):
     @classmethod
     def _get_kwargs_from_dict(cls, as_dict):
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'allow_point_select': as_dict.pop('allowPointSelect', None),
-            'animation': as_dict.pop('animation', None),
-            'class_name': as_dict.pop('className', None),
-            'clip': as_dict.pop('clip', None),
-            'color': as_dict.pop('color', None),
-            'cursor': as_dict.pop('cursor', None),
-            'custom': as_dict.pop('custom', None),
-            'dash_style': as_dict.pop('dashStyle', None),
-            'data_labels': as_dict.pop('dataLabels', None),
-            'description': as_dict.pop('description', None),
-            'enable_mouse_tracking': as_dict.pop('enableMouseTracking', None),
-            'events': as_dict.pop('events', None),
-            'include_in_data_export': as_dict.pop('includeInDataExport', None),
-            'keys': as_dict.pop('keys', None),
-            'label': as_dict.pop('label', None),
-            'linked_to': as_dict.pop('linkedTo', None),
-            'marker': as_dict.pop('marker', None),
-            'on_point': as_dict.pop('onPoint', None),
-            'opacity': as_dict.pop('opacity', None),
-            'point': as_dict.pop('point', None),
-            'point_description_formatter': as_dict.pop('pointDescriptionFormatter', None),
-            'selected': as_dict.pop('selected', None),
-            'show_checkbox': as_dict.pop('showCheckbox', None),
-            'show_in_legend': as_dict.pop('showInLegend', None),
-            'skip_keyboard_navigation': as_dict.pop('skipKeyboardNavigation', None),
-            'states': as_dict.pop('states', None),
-            'threshold': as_dict.pop('threshold', None),
-            'tooltip': as_dict.pop('tooltip', None),
-            'turbo_threshold': as_dict.pop('turboThreshold', None),
-            'visible': as_dict.pop('visible', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'allow_point_select': as_dict.get('allowPointSelect', None),
+            'animation': as_dict.get('animation', None),
+            'class_name': as_dict.get('className', None),
+            'clip': as_dict.get('clip', None),
+            'color': as_dict.get('color', None),
+            'cursor': as_dict.get('cursor', None),
+            'custom': as_dict.get('custom', None),
+            'dash_style': as_dict.get('dashStyle', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'description': as_dict.get('description', None),
+            'enable_mouse_tracking': as_dict.get('enableMouseTracking', None),
+            'events': as_dict.get('events', None),
+            'include_in_data_export': as_dict.get('includeInDataExport', None),
+            'keys': as_dict.get('keys', None),
+            'label': as_dict.get('label', None),
+            'linked_to': as_dict.get('linkedTo', None),
+            'marker': as_dict.get('marker', None),
+            'on_point': as_dict.get('onPoint', None),
+            'opacity': as_dict.get('opacity', None),
+            'point': as_dict.get('point', None),
+            'point_description_formatter': as_dict.get('pointDescriptionFormatter', None),
+            'selected': as_dict.get('selected', None),
+            'show_checkbox': as_dict.get('showCheckbox', None),
+            'show_in_legend': as_dict.get('showInLegend', None),
+            'skip_keyboard_navigation': as_dict.get('skipKeyboardNavigation', None),
+            'states': as_dict.get('states', None),
+            'sticky_tracking': as_dict.get('stickyTracking', None),
+            'threshold': as_dict.get('threshold', None),
+            'tooltip': as_dict.get('tooltip', None),
+            'turbo_threshold': as_dict.get('turboThreshold', None),
+            'visible': as_dict.get('visible', None),
 
-            'border_color': as_dict.pop('borderColor', None),
-            'border_width': as_dict.pop('borderWidth', None),
-            'center': as_dict.pop('center', None),
-            'center_in_category': as_dict.pop('centerInCategory', None),
-            'color_by_point': as_dict.pop('colorByPoint', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'colors': as_dict.pop('colors', None),
-            'curve_factor': as_dict.pop('curveFactor', None),
-            'levels': as_dict.pop('levels', None),
-            'link_opacity': as_dict.pop('linkOpacity', None),
-            'min_link_width': as_dict.pop('minLinkWidth', None),
-            'node_padding': as_dict.pop('nodePadding', None),
-            'node_width': as_dict.pop('nodeWidth', None),
-            'start_angle': as_dict.pop('startAngle', None)
+            'border_color': as_dict.get('borderColor', None),
+            'border_width': as_dict.get('borderWidth', None),
+            'center': as_dict.get('center', None),
+            'center_in_category': as_dict.get('centerInCategory', None),
+            'color_by_point': as_dict.get('colorByPoint', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'colors': as_dict.get('colors', None),
+            'curve_factor': as_dict.get('curveFactor', None),
+            'levels': as_dict.get('levels', None),
+            'link_opacity': as_dict.get('linkOpacity', None),
+            'min_link_width': as_dict.get('minLinkWidth', None),
+            'node_padding': as_dict.get('nodePadding', None),
+            'node_width': as_dict.get('nodeWidth', None),
+            'start_angle': as_dict.get('startAngle', None)
         }
 
         return kwargs
