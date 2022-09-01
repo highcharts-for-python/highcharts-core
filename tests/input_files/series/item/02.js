bffc0f4cd177a5f4a82bfdffa262c15a8c8ad03c
Chris Modzelewski <software@insightindustry.com>
2022-09-01 12:15:54 -0400

    Implemented unit tests for highcharts.series.item.

--- tests/input_files/series/item/02.js
+++ tests/input_files/series/item/02.js
@@ -1,4 +1,120 @@
 {
+  data: [
+    {
+      y: 123,
+      dataLabels: {
+        align: 'center',
+        allowOverlap: true,
+        animation: {
+            defer: 5
+        },
+        backgroundColor: {
+            linearGradient: {
+                x1: 0.123,
+                x2: 0.234,
+                y1: 0.345,
+                y2: 0.456
+            },
+            stops: [
+                [0.12, '#999'],
+                [0.34, '#fff']
+            ]
+        },
+        borderColor: '#999999',
+        borderRadius: 24,
+        borderWidth: 1,
+        className: 'some-class-name',
+        color: '#000000',
+        crop: true,
+        defer: false,
+        enabled: true,
+        filter: {
+            operator: '>=',
+            property: 'some_property',
+            value: 123
+        },
+        format: 'some format',
+        formatter: function() { return true; },
+        inside: true,
+        nullFormat: 'some format',
+        nullFormatter: function() { return true; },
+        overflow: 'none',
+        padding: 12,
+        position: 'center',
+        rotation: 0,
+        shadow: false,
+        shape: 'rect',
+        style: 'style goes here',
+        useHTML: false,
+        verticalAlign: 'top',
+        x: 10,
+        y: 20,
+        z: 0
+      },
+      dragDrop: {
+          draggableX: true,
+          draggableY: true,
+          dragHandle: {
+              className: 'draghandle-classname-goes-here',
+              color: '#ccc',
+              cursor: 'alias',
+              lineColor: '#ddd',
+              lineWidth: 2,
+              pathFormatter: function() { return true; },
+              zIndex: 10
+          },
+          dragMaxX: 3456,
+          dragMaxY: 6532,
+          dragMinX: 123,
+          dragMinY: 321,
+          dragPrecisionX: 5,
+          dragPrecisionY: 5,
+          dragSensitivity: 2,
+          groupBy: 'some-property-name',
+          guideBox: {
+              default: {
+                  className: 'some-classname-goes-here',
+                  color: '#999',
+                  cursor: 'pointer',
+                  lineColor: '#ccc',
+                  lineWidth: 2,
+                  zIndex: 100
+              }
+          },
+          liveRedraw: true
+      },
+      drilldown: 'some-id-goes-here',
+
+      accessibility: {
+          description: 'Some description goes here',
+          enabled: true
+      },
+      className: 'some-class-name',
+      color: '#ccc',
+      colorIndex: 2,
+      custom: {
+          'some_key': 123,
+          'other_key': 456
+      },
+      description: 'Some description goes here',
+      events: {
+        click: function(event) { return true; },
+        drag: function(event) { return true; },
+        drop: function(event) { return true; },
+        mouseOut: function(event) { return true; }
+      },
+      id: 'some-id-goes-here',
+      labelrank: 3,
+      name: 'Some Name Goes here',
+      selected: false
+    }
+  ],
+
+  colorAxis: 1,
+  colorIndex: 3,
+  colorKey: 'some-key-goes-here',
+  linecap: 'round',
+
   itemPadding: 12,
   layout: 'horizontal',
   rows: 3,
@@ -6,9 +122,6 @@
   borderColor: '#ccc',
   borderWidth: 1,
   center: ['50%', '50%'],
-  colorAxis: 1,
-  colorIndex: 3,
-  colorKey: 'some-key-goes-here',
   colors: [
       '#fff',
       'ccc',
@@ -46,7 +159,6 @@
   fillColor: '#fff',
   ignoreHiddenPoint: true,
   innerSize: '30%',
-  linecap: 'round',
   minSize: '20%',
   size: 80,
   slicedOffset: 24,
