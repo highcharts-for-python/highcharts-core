d85114a690082761d6b292668404f20b0c49760b
Chris Modzelewski <software@insightindustry.com>
2022-09-01 10:16:41 -0400

    Implemented unit tests for highcharts.series.data.single_point.

--- highcharts/series/data/single_point.py
+++ highcharts/series/data/single_point.py
@@ -18,9 +18,9 @@ class SinglePointBase(DataBase):
         self._drag_drop = None
         self._drilldown = None
 
-        self.data_labels = kwargs.pop('data_labels', None)
-        self.drag_drop = kwargs.pop('drag_drop', None)
-        self.drilldown = kwargs.pop('drilldown', None)
+        self.data_labels = kwargs.get('data_labels', None)
+        self.drag_drop = kwargs.get('drag_drop', None)
+        self.drilldown = kwargs.get('drilldown', None)
 
         super().__init__(**kwargs)
 
@@ -86,27 +86,33 @@ class SinglePointBase(DataBase):
 
         """
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'class_name': as_dict.pop('className', None),
-            'color': as_dict.pop('color', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'custom': as_dict.pop('custom', None),
-            'description': as_dict.pop('description', None),
-            'events': as_dict.pop('events', None),
-            'id': as_dict.pop('id', None),
-            'label_rank': as_dict.pop('labelrank', None),
-            'name': as_dict.pop('name', None),
-            'selected': as_dict.pop('selected', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'class_name': as_dict.get('className', None),
+            'color': as_dict.get('color', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'custom': as_dict.get('custom', None),
+            'description': as_dict.get('description', None),
+            'events': as_dict.get('events', None),
+            'id': as_dict.get('id', None),
+            'label_rank': as_dict.get('labelrank',
+                                      None) or as_dict.get('labelRank',
+                                                           None),
+            'name': as_dict.get('name', None),
+            'selected': as_dict.get('selected', None),
 
-            'data_labels': as_dict.pop('dataLabels', None),
-            'drag_drop': as_dict.pop('dragDrop', None),
-            'drilldown': as_dict.pop('drilldown', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'drilldown': as_dict.get('drilldown', None),
         }
 
         return kwargs
 
     def _to_untrimmed_dict(self, in_cls = None) -> dict:
         untrimmed = {
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
             'accessibility': self.accessibility,
             'className': self.class_name,
             'color': self.color,
@@ -119,9 +125,6 @@ class SinglePointBase(DataBase):
             'name': self.name,
             'selected': self.selected,
 
-            'dataLabels': self.data_labels,
-            'dragDrop': self.drag_drop,
-            'drilldown': self.drilldown,
         }
 
         return untrimmed
@@ -133,7 +136,7 @@ class SinglePointData(SinglePointBase):
     def __init__(self, **kwargs):
         self._y = None
 
-        self.y = kwargs.pop('y', None)
+        self.y = kwargs.get('y', None)
 
         super().__init__(**kwargs)
 
@@ -199,29 +202,37 @@ class SinglePointData(SinglePointBase):
 
         """
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'class_name': as_dict.pop('className', None),
-            'color': as_dict.pop('color', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'custom': as_dict.pop('custom', None),
-            'description': as_dict.pop('description', None),
-            'events': as_dict.pop('events', None),
-            'id': as_dict.pop('id', None),
-            'label_rank': as_dict.pop('labelrank', None),
-            'name': as_dict.pop('name', None),
-            'selected': as_dict.pop('selected', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'class_name': as_dict.get('className', None),
+            'color': as_dict.get('color', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'custom': as_dict.get('custom', None),
+            'description': as_dict.get('description', None),
+            'events': as_dict.get('events', None),
+            'id': as_dict.get('id', None),
+            'label_rank': as_dict.get('labelrank',
+                                      None) or as_dict.get('labelRank',
+                                                           None),
+            'name': as_dict.get('name', None),
+            'selected': as_dict.get('selected', None),
 
-            'data_labels': as_dict.pop('dataLabels', None),
-            'drag_drop': as_dict.pop('dragDrop', None),
-            'drilldown': as_dict.pop('drilldown', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'drilldown': as_dict.get('drilldown', None),
 
-            'y': as_dict.pop('y', None),
+            'y': as_dict.get('y', None),
         }
 
         return kwargs
 
     def _to_untrimmed_dict(self, in_cls = None) -> dict:
         untrimmed = {
+            'y': self.y,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
             'accessibility': self.accessibility,
             'className': self.class_name,
             'color': self.color,
@@ -234,11 +245,6 @@ class SinglePointData(SinglePointBase):
             'name': self.name,
             'selected': self.selected,
 
-            'dataLabels': self.data_labels,
-            'dragDrop': self.drag_drop,
-            'drilldown': self.drilldown,
-
-            'y': self.y,
         }
 
         return untrimmed
@@ -250,7 +256,7 @@ class SingleValueData(SinglePointBase):
     def __init__(self, **kwargs):
         self._value = None
 
-        self.value = kwargs.pop('value', None)
+        self.value = kwargs.get('value', None)
 
         super().__init__(**kwargs)
 
@@ -316,28 +322,36 @@ class SingleValueData(SinglePointBase):
 
         """
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'class_name': as_dict.pop('className', None),
-            'color': as_dict.pop('color', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'custom': as_dict.pop('custom', None),
-            'description': as_dict.pop('description', None),
-            'events': as_dict.pop('events', None),
-            'id': as_dict.pop('id', None),
-            'label_rank': as_dict.pop('labelrank', None),
-            'name': as_dict.pop('name', None),
-            'selected': as_dict.pop('selected', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'class_name': as_dict.get('className', None),
+            'color': as_dict.get('color', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'custom': as_dict.get('custom', None),
+            'description': as_dict.get('description', None),
+            'events': as_dict.get('events', None),
+            'id': as_dict.get('id', None),
+            'label_rank': as_dict.get('labelrank',
+                                      None) or as_dict.get('labelRank',
+                                                           None),
+            'name': as_dict.get('name', None),
+            'selected': as_dict.get('selected', None),
 
-            'data_labels': as_dict.pop('dataLabels', None),
-            'drag_drop': as_dict.pop('dragDrop', None),
-            'drilldown': as_dict.pop('drilldown', None),
-            'value': as_dict.pop('value', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'drilldown': as_dict.get('drilldown', None),
+            'value': as_dict.get('value', None),
         }
 
         return kwargs
 
     def _to_untrimmed_dict(self, in_cls = None) -> dict:
         untrimmed = {
+            'value': self.value,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
             'accessibility': self.accessibility,
             'className': self.class_name,
             'color': self.color,
@@ -350,10 +364,6 @@ class SingleValueData(SinglePointBase):
             'name': self.name,
             'selected': self.selected,
 
-            'dataLabels': self.data_labels,
-            'dragDrop': self.drag_drop,
-            'drilldown': self.drilldown,
-            'value': self.value,
         }
 
         return untrimmed
@@ -365,7 +375,7 @@ class SingleXData(SinglePointBase):
     def __init__(self, **kwargs):
         self._x = None
 
-        self.x = kwargs.pop('x', None)
+        self.x = kwargs.get('x', None)
 
         super().__init__(**kwargs)
 
@@ -376,7 +386,7 @@ class SingleXData(SinglePointBase):
 
         :rtype: numeric or :class:`EnforcedNullType` or :obj:`None <python:None>`
         """
-        return self._y
+        return self._x
 
     @x.setter
     def x(self, value):
@@ -431,29 +441,37 @@ class SingleXData(SinglePointBase):
 
         """
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'class_name': as_dict.pop('className', None),
-            'color': as_dict.pop('color', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'custom': as_dict.pop('custom', None),
-            'description': as_dict.pop('description', None),
-            'events': as_dict.pop('events', None),
-            'id': as_dict.pop('id', None),
-            'label_rank': as_dict.pop('labelrank', None),
-            'name': as_dict.pop('name', None),
-            'selected': as_dict.pop('selected', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'class_name': as_dict.get('className', None),
+            'color': as_dict.get('color', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'custom': as_dict.get('custom', None),
+            'description': as_dict.get('description', None),
+            'events': as_dict.get('events', None),
+            'id': as_dict.get('id', None),
+            'label_rank': as_dict.get('labelrank',
+                                      None) or as_dict.get('labelRank',
+                                                           None),
+            'name': as_dict.get('name', None),
+            'selected': as_dict.get('selected', None),
 
-            'data_labels': as_dict.pop('dataLabels', None),
-            'drag_drop': as_dict.pop('dragDrop', None),
-            'drilldown': as_dict.pop('drilldown', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'drilldown': as_dict.get('drilldown', None),
 
-            'x': as_dict.pop('x', None),
+            'x': as_dict.get('x', None),
         }
 
         return kwargs
 
     def _to_untrimmed_dict(self, in_cls = None) -> dict:
         untrimmed = {
+            'x': self.x,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
             'accessibility': self.accessibility,
             'className': self.class_name,
             'color': self.color,
@@ -466,23 +484,18 @@ class SingleXData(SinglePointBase):
             'name': self.name,
             'selected': self.selected,
 
-            'dataLabels': self.data_labels,
-            'dragDrop': self.drag_drop,
-            'drilldown': self.drilldown,
-
-            'x': self.x,
         }
 
         return untrimmed
 
 
-class LabeledSingleXData(SinglePointBase):
+class LabeledSingleXData(SingleXData):
     """Data point that features a single labeled ``x`` value."""
 
     def __init__(self, **kwargs):
         self._label = None
 
-        self.label = kwargs.pop('label', None)
+        self.label = kwargs.get('label', None)
 
         super().__init__(**kwargs)
 
@@ -544,30 +557,39 @@ class LabeledSingleXData(SinglePointBase):
 
         """
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'class_name': as_dict.pop('className', None),
-            'color': as_dict.pop('color', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'custom': as_dict.pop('custom', None),
-            'description': as_dict.pop('description', None),
-            'events': as_dict.pop('events', None),
-            'id': as_dict.pop('id', None),
-            'label_rank': as_dict.pop('labelrank', None),
-            'name': as_dict.pop('name', None),
-            'selected': as_dict.pop('selected', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'class_name': as_dict.get('className', None),
+            'color': as_dict.get('color', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'custom': as_dict.get('custom', None),
+            'description': as_dict.get('description', None),
+            'events': as_dict.get('events', None),
+            'id': as_dict.get('id', None),
+            'label_rank': as_dict.get('labelrank',
+                                      None) or as_dict.get('labelRank',
+                                                           None),
+            'name': as_dict.get('name', None),
+            'selected': as_dict.get('selected', None),
 
-            'data_labels': as_dict.pop('dataLabels', None),
-            'drag_drop': as_dict.pop('dragDrop', None),
-            'drilldown': as_dict.pop('drilldown', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'drilldown': as_dict.get('drilldown', None),
 
-            'label': as_dict.pop('label', None),
-            'x': as_dict.pop('x', None),
+            'label': as_dict.get('label', None),
+            'x': as_dict.get('x', None),
         }
 
         return kwargs
 
     def _to_untrimmed_dict(self, in_cls = None) -> dict:
         untrimmed = {
+            'label': self.label,
+            'x': self.x,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
             'accessibility': self.accessibility,
             'className': self.class_name,
             'color': self.color,
@@ -580,12 +602,7 @@ class LabeledSingleXData(SinglePointBase):
             'name': self.name,
             'selected': self.selected,
 
-            'dataLabels': self.data_labels,
-            'dragDrop': self.drag_drop,
-            'drilldown': self.drilldown,
 
-            'label': self.label,
-            'x': self.x,
         }
 
         return untrimmed
@@ -598,8 +615,8 @@ class ConnectedSingleXData(SingleXData):
         self._connector_color = None
         self._connector_width = None
 
-        self.connector_color = kwargs.pop('connector_color', None)
-        self.connector_width = kwargs.pop('connector_width', None)
+        self.connector_color = kwargs.get('connector_color', None)
+        self.connector_width = kwargs.get('connector_width', None)
 
         super().__init__(**kwargs)
 
@@ -676,32 +693,43 @@ class ConnectedSingleXData(SingleXData):
 
         """
         kwargs = {
-            'accessibility': as_dict.pop('accessibility', None),
-            'class_name': as_dict.pop('className', None),
-            'color': as_dict.pop('color', None),
-            'color_index': as_dict.pop('colorIndex', None),
-            'custom': as_dict.pop('custom', None),
-            'description': as_dict.pop('description', None),
-            'events': as_dict.pop('events', None),
-            'id': as_dict.pop('id', None),
-            'label_rank': as_dict.pop('labelrank', None),
-            'name': as_dict.pop('name', None),
-            'selected': as_dict.pop('selected', None),
+            'accessibility': as_dict.get('accessibility', None),
+            'class_name': as_dict.get('className', None),
+            'color': as_dict.get('color', None),
+            'color_index': as_dict.get('colorIndex', None),
+            'custom': as_dict.get('custom', None),
+            'description': as_dict.get('description', None),
+            'events': as_dict.get('events', None),
+            'id': as_dict.get('id', None),
+            'label_rank': as_dict.get('labelrank',
+                                      None) or as_dict.get('labelRank',
+                                                           None),
+            'name': as_dict.get('name', None),
+            'selected': as_dict.get('selected', None),
 
-            'data_labels': as_dict.pop('dataLabels', None),
-            'drag_drop': as_dict.pop('dragDrop', None),
-            'drilldown': as_dict.pop('drilldown', None),
+            'data_labels': as_dict.get('dataLabels', None),
+            'drag_drop': as_dict.get('dragDrop', None),
+            'drilldown': as_dict.get('drilldown', None),
 
-            'x': as_dict.pop('x', None),
+            'x': as_dict.get('x', None),
 
-            'connector_color': as_dict.pop('connectorColor', None),
-            'connector_width': as_dict.pop('connectorWidth', None),
+            'connector_color': as_dict.get('connectorColor', None),
+            'connector_width': as_dict.get('connectorWidth', None),
         }
 
         return kwargs
 
     def _to_untrimmed_dict(self, in_cls = None) -> dict:
         untrimmed = {
+            'connectorColor': self.connector_color,
+            'connectorWidth': self.connector_width,
+
+            'x': self.x,
+
+            'dataLabels': self.data_labels,
+            'dragDrop': self.drag_drop,
+            'drilldown': self.drilldown,
+
             'accessibility': self.accessibility,
             'className': self.class_name,
             'color': self.color,
@@ -713,15 +741,6 @@ class ConnectedSingleXData(SingleXData):
             'labelrank': self.label_rank,
             'name': self.name,
             'selected': self.selected,
-
-            'dataLabels': self.data_labels,
-            'dragDrop': self.drag_drop,
-            'drilldown': self.drilldown,
-
-            'x': self.x,
-
-            'connectorColor': self.connector_color,
-            'connectorWidth': self.connector_width,
         }
 
         return untrimmed
