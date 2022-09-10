468ea398248efed49c1df0080ee33f0720f1b472
Chris Modzelewski <software@insightindustry.com>
2022-09-09 20:13:48 -0400

    Implemented a convenience module for easy importing.

---
+++ highcharts_python/highcharts.py
@@ -0,0 +1,9 @@
+"""Convenience module that imports all classes from the entire library."""
+
+from highcharts_python.chart import Chart
+
+from highcharts_python.headless_export import ExportServer
+
+from highcharts_python import global_options, options, utility_classes
+
+SharedOptions = global_options.shared_options.SharedOptions
