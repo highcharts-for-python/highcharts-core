6708c53f9f96d95387fb5eac8e4d7b579b472b06
Chris Modzelewski <chris@highchartspython.com>
2023-04-11 16:44:43 -0400

    Prep for v.1.0.0 release.

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ intersphinx_mapping = {
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False
 
 # Inheritance Diagram configuration
 inheritance_graph_attrs = {
