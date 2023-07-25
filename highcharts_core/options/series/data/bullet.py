79da938bf1d6e0cf52ff5231c8bb5243719b49ee
Chris Modzelewski <chris@highchartspython.com>
2023-07-25 10:42:19 -0400

    Updated .from_array() to support category naming. Closes #67.

--- highcharts_core/options/series/data/bullet.py
+++ highcharts_core/options/series/data/bullet.py
@@ -85,8 +85,10 @@ class BulletData(BarData):
                     raise errors.HighchartsValueError(f'data expects either a 3D or 2D '
                                                       f'collection. Collection received '
                                                       f'had {len(item)} dimensions.')
-
+                    
                 as_obj = cls.from_dict(as_dict)
+                if checkers.is_string(as_obj.x):
+                    as_obj.name = as_obj.x
             else:
                 raise errors.HighchartsValueError(f'each data point supplied must either '
                                                   f'be a Bullet Data Point or be '
